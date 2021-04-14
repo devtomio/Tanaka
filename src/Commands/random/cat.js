07558b540e65c9da9da0061238de6d2e9696d92c
1chiSensei <mail@1chi.tk>
2021-04-14 23:26:36 +0800

    feat(commands): add cat command

--- src/Commands/random/cat.js
+++ src/Commands/random/cat.js
@@ -1,15 +1,15 @@
 const { Command } = require('discord.js-commando');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
 const request = require('node-superfetch');
 
-module.exports = class FactCommand extends Command {
+module.exports = class CatCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'fact',
-			aliases: ['random-fact'],
+			name: 'cat',
+			aliases: ['random-cat'],
 			group: 'random',
-			memberName: 'fact',
-			description: 'Responds with a random fact.',
+			memberName: 'cat',
+			description: 'Responds with a random cat image.',
 			clientPermissions: ['EMBED_LINKS'],
 			throttling: {
 				usages: 5,
@@ -22,12 +22,11 @@ module.exports = class FactCommand extends Command {
 		const message = await msg.embed({ description: 'Fetching....' });
 
 		const { body } = await request
-			.get('https://uselessfacts.jsph.pl/random.json')
-			.query({ language: 'en' })
+			.get('https://some-random-api.ml/img/cat')
 			.set({ 'User-Agent': 'TanakaBot 1.0.0 (https://github.com/1chiSensei/Tanaka)' });
 
 		const embed = new MessageEmbed()
-			.setDescription(`\`\`\`\n${body.text}\n\`\`\``)
+			.setImage(new MessageAttachment(body.link, 'cat.png'))
 			.setFooter(
 				`Requested by ${msg.author.tag}`,
 				msg.author.displayAvatarURL({ dynamic: true, size: 4096 }),
