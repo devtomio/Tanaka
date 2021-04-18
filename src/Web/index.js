03cbba4c745e7892fbae5c42bab308e919f75acd
1chiSensei <mail@1chi.tk>
2021-04-18 16:32:23 +0800

    feat(web): use `@2pg/oauth` instead of `disco-oauth`

--- src/Web/index.js
+++ src/Web/index.js
@@ -1,21 +1,23 @@
-const OAuthClient = require('disco-oauth');
+const { Client } = require('@2pg/oauth');
 const express = require('express');
+const cookies = require('cookies');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const client = new OAuthClient(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-	.setRedirect('https://tanaka-production.up.railway.app/auth/callback')
-	.setScopes('identify');
-
-const uri =
-	'https://discord.com/api/oauth2/authorize?client_id=804605929944645672&redirect_uri=https%3A%2F%2Ftanaka-production.up.railway.app%2Fauth%2Fcallback&response_type=code&scope=identify';
+const client = new Client({
+	id: process.env.CLIENT_ID,
+	secret: process.env.CLIENT_SECRET,
+	redirectURI: 'https://tanaka-production.up.railway.app/auth/callback',
+	scopes: ['identify'],
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
+app.use(cookies.express(['I', 'store', 'login', 'sessions.']));
 app.use(morgan('combined'));
 
 app.set('view engine', 'ejs');
@@ -24,35 +26,23 @@ app.set('json spaces', 8);
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
-	if (req.cookies?.discordToken) {
-		const data = await client.getUser(req.cookies.discordToken);
-		return res.status(200).render('index', { data });
-	}
+	const key = req.cookies.get('discordToken');
 
-	return res.status(200).render('index', { data: null });
+	res.render('index', { data: key ? await client.getUser(key) : null });
 });
 
-app.get('/auth/login', (req, res) => {
-	if (req.cookies?.discordToken) return res.redirect(302, '/');
-
-	return res.redirect(302, uri);
-});
+app.get('/auth/login', (_, res) => res.redirect(302, client.authCodeLink.url));
 
 app.get('/auth/callback', async (req, res) => {
-	if (req.cookies?.discordToken) return res.redirect(302, '/');
+	const key = await client.getAccess(req.query.code.toString());
 
-	const accessCode = req.query.code;
-	const accessToken = await client.getAccess(accessCode);
-
-	res.cookie('discordToken', accessToken, { maxAge: 604800000 });
-
-	return res.redirect(302, '/');
+	res.cookie('discordToken', key).redirect('/');
 });
 
 app.get('/auth/logout', (_, res) => {
-	res.cookie('discordToken', null, { maxAge: -1 });
+	res.clearCookie('discordToken', undefined);
 
-	return res.redirect(302, '/');
+	res.redirect(302, '/');
 });
 
 module.exports = () => app.listen(process.env.PORT, () => console.log('Website is ready!'));
