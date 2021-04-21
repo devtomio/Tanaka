de830ad9d0f682d38721768ac79f9da59c366c55
1chiSensei <mail@1chi.tk>
2021-04-21 11:04:12 +0800

    fix(commands): use canvacord

--- src/Commands/img/triggered.js
+++ src/Commands/img/triggered.js
@@ -1,13 +1,5 @@
-const { drawImageWithTint } = require('../../Structures/Canvas');
-const { streamToArray } = require('../../Structures/Util');
-const { createCanvas, loadImage } = require('canvas');
 const { Command } = require('discord.js-commando');
-const request = require('node-superfetch');
-const GIFEncoder = require('gifencoder');
-const path = require('path');
-
-const coord1 = [-25, -33, -42, -14];
-const coord2 = [-25, -13, -34, -10];
+const { Canvas } = require('canvacord');
 
 module.exports = class TriggeredCommand extends Command {
 	constructor(client) {
@@ -34,36 +26,9 @@ module.exports = class TriggeredCommand extends Command {
 
 	async run(msg, { image }) {
 		try {
-			const base = await loadImage(
-				path.join(__dirname, '..', '..', 'Assets', 'image', 'triggered.png'),
-			);
-			const { body } = await request.get(image);
-			const avatar = await loadImage(body);
-			const encoder = new GIFEncoder(base.width, base.width);
-			const canvas = createCanvas(base.width, base.width);
-			const ctx = canvas.getContext('2d');
+			const attachment = await Canvas.trigger(image);
 
-			ctx.fillStyle = 'white';
-			ctx.fillRect(0, 0, base.width, base.width);
-
-			const stream = encoder.createReadStream();
-
-			encoder.start();
-			encoder.setRepeat(0);
-			encoder.setDelay(50);
-			encoder.setQuality(200);
-
-			for (let i = 0; i < 4; i++) {
-				drawImageWithTint(ctx, avatar, 'red', coord1[i], coord2[i], 300, 300);
-				ctx.drawImage(base, 0, 218, 256, 38);
-				encoder.addFrame(ctx);
-			}
-
-			encoder.finish();
-
-			const buffer = await streamToArray(stream);
-
-			return msg.say({ files: [{ attachment: Buffer.concat(buffer), name: 'triggered.gif' }] });
+			return msg.say({ files: [{ attachment, name: 'triggered.gif' }] });
 		} catch (err) {
 			return msg.reply(`An error occured: \`${err.message}\`.`);
 		}
