84b82986a1603a90f79c5af21b37da5a84c64f19
1chiSensei <mail@1chi.tk>
2021-04-23 10:13:30 +0800

    feat(puppeteer): use the next version

--- src/Structures/Puppeteer.js
+++ src/Structures/Puppeteer.js
@@ -3,7 +3,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const puppeteer = require('puppeteer-extra');
 
 puppeteer.use(StealthPlugin());
-puppeteer.use(AdblockPlugin());
+puppeteer.use(AdblockPlugin({ blockTrackers: true }));
 
 const screenshot = async (url) => {
 	const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
@@ -13,7 +13,7 @@ const screenshot = async (url) => {
 		width: 1920,
 		height: 1080,
 	});
-	await page.goto(url);
+	await page.goto(url, { waitUntil: 'domcontentloaded' });
 
 	const buffer = await page.screenshot();
 
