a98f85c98a7997b30a5dca70629cadf5964c7815
Yoshida Tomio <mail@1chi.tk>
2021-04-24 07:33:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 # Start Lavalink
 pm2 start Lavalink.jar --name lavalink --interpreter java --interpreter-args "-jar"
 
-# Start Bot
+# Start Bot and Build the Files
 yarn build
 dotenv -- pm2-runtime start dist/index.js --name tanaka
