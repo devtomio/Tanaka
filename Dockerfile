1a325e039fdb3053d8ace018752aa83fad7abafb
1chiSensei <mail@1chi.tk>
2021-04-25 13:47:32 +0800

    fix(puppeteer): fix something

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,6 @@ RUN apt-get update \
 	&& curl -sL https://deb.nodesource.com/setup_16.x | bash - \
 	&& apt-get update \
 	&& apt-get install -y nodejs default-jre default-jdk libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev ffmpeg libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb libgbm-dev \
-	&& export JAVA_HOME=/usr/lib/jvm/openjdk-13-jdk \
-	&& export PATH=$PATH:$JAVA_HOME/bin \
 	&& npm i -g yarn \
 	&& yarn global add pm2 dotenv-cli node-gyp \
 	&& yarn \
--- src/Structures/Puppeteer.js
+++ src/Structures/Puppeteer.js
@@ -7,9 +7,16 @@ puppeteer.use(AdblockPlugin({ blockTrackers: true }));
 
 const screenshot = async (url) => {
 	try {
+		const headers = {
+			'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,it;q=0.5',
+			'accept-encoding': 'gzip, deflate, br',
+			accept:
+				'text/html,application/xhtml+xml,application/json,text/plain,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+		};
+
 		const browser = await puppeteer.launch({
 			headless: true,
-			args: ['--no-sandbox', '--disable-gpu', '--single-process'],
+			args: ['--no-sandbox', '--disable-gpu', '--single-process', '--disable-dev-shm-usage'],
 		});
 		const page = await browser.newPage();
 
@@ -17,6 +24,7 @@ const screenshot = async (url) => {
 			width: 1920,
 			height: 1080,
 		});
+		await page.setExtraHTTPHeaders(headers);
 		await page.goto(url, { waitUntil: ['load', 'domcontentloaded'] });
 		await page.waitForTimeout(5000);
 
