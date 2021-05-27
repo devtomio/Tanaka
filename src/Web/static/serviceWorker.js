09d8a386e2ce6dc74d52233b08bef087fde6d1fd
1chiSensei <mail@1chi.tk>
2021-05-27 15:24:43 +0800

    fix(web): fix service worker

--- src/Web/static/serviceWorker.js
+++ src/Web/static/serviceWorker.js
@@ -1,67 +1,66 @@
 /* eslint-disable */
 
-const version = 'v1::';
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+	'/',
+	'/static/ackee.js',
+	'/static/color-mode.js',
+	'/static/modesta.js',
+	'/static/script.js',
+	'/static/typed.js',
+	'/static/modesta.css',
+	'/static/navbar.css',
+	'/static/style.css',
+];
 
 self.addEventListener('install', (event) => {
-	console.log('[ServiceWorker]: Install in progress...');
-
 	event.waitUntil(
 		caches
-			.open(`${version}fundamentals`)
-			.then((cache) => {
-				return cache.addAll([
-					'/',
-					'/static/ackee.js',
-					'/static/color-mode.js',
-					'/static/modesta.js',
-					'/static/script.js',
-					'/static/typed.js',
-					'/static/modesta.css',
-					'/static/navbar.css',
-					'/static/style.css',
-				]);
+			.open(PRECACHE)
+			.then((cache) => cache.addAll(PRECACHE_URLS))
+			.then(self.skipWaiting()),
+	);
+});
+
+self.addEventListener('activate', (event) => {
+	const currentCaches = [PRECACHE, RUNTIME];
+	event.waitUntil(
+		caches
+			.keys()
+			.then((cacheNames) => {
+				return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
 			})
-			.then(() => console.log('[ServiceWorker]: Install complete!')),
+			.then((cachesToDelete) => {
+				return Promise.all(
+					cachesToDelete.map((cacheToDelete) => {
+						return caches.delete(cacheToDelete);
+					}),
+				);
+			})
+			.then(() => self.clients.claim()),
 	);
 });
 
 self.addEventListener('fetch', (event) => {
-	console.log('[ServiceWorker]: Fetch in progress...');
+	if (event.request.url.startsWith(self.location.origin)) {
+		event.respondWith(
+			caches.match(event.request).then((cachedResponse) => {
+				if (cachedResponse) {
+					return cachedResponse;
+				}
 
-	if (event.request.method !== 'GET')
-		return console.log(`[ServiceWorker]: Blocked a request.\n${event.request.method} ${event.request.url}`);
-
-	event.respondWith(
-		caches.match(event.request).then((cached) => {
-			const fetchedFromNetwork = (response) => {
-				const cacheCopy = response.clone();
-
-				console.log(`[ServiceWorker]: Response from network.\n${event.request.url}`);
-
-				caches.open(`${version}pages`)
-					.then((cache) => cache.put(event.request, cacheCopy))
-					.then(() =>
-						console.log(
-							`[ServiceWorker]: Response stored in cache.\n${event.request.url}`,
-						),
-					);
-
-				return response;
-			};
-
-			const unableToResolve = () => {
-				console.log(`[ServiceWorker]: Fetch request failed.`);
-
-				return new Response('<h1>Service Unavailable</h1>', {
-					status: 503,
-					statusText: 'Service Unavailable',
-					headers: new Headers({
-						'Content-Type': 'text/html',
-					}),
+				return caches.open(RUNTIME).then((cache) => {
+					return fetch(event.request).then((response) => {
+						// Put a copy of the response in the runtime cache.
+						return cache.put(event.request, response.clone()).then(() => {
+							return response;
+						});
+					});
 				});
-			};
-
-			const networked = fetch(event.request).then(fetchedFromNetwork).catch(unableToResolve);
-		}),
-	);
+			}),
+		);
+	}
 });
